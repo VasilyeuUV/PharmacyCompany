973d17fe66c035a0432d7ad8f0e305e3f48a55a0
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-06-22 01:44:27 +0300

    entity: new project & base class

---
+++ PharmCompany.Entities/BaseEntities/ANameableEntityBase.cs
@@ -0,0 +1,28 @@
+﻿using PharmCompany.Entities.Capabilities;
+
+namespace PharmCompany.Entities.BaseEntities
+{
+    /// <summary>
+    /// Базовая именованная сущность
+    /// </summary>
+    public abstract class ANameableEntityBase : IIdentifiable, INameable
+    {
+
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        protected ANameableEntityBase(string name, Guid guid)
+        {
+
+            this.Name = name;
+            this.Id = guid == Guid.Empty
+                ? Guid.NewGuid()
+                : guid;
+        }
+
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+    }
+}
---
+++ PharmCompany.Entities/Capabilities/IIdentifiable.cs
@@ -0,0 +1,13 @@
+﻿namespace PharmCompany.Entities.Capabilities
+{
+    /// <summary>
+    /// Идентифицируемый
+    /// </summary>
+    public interface IIdentifiable
+    {
+        /// <summary>
+        /// Идентфиикатор
+        /// </summary>
+        Guid Id { get; }
+    }
+}
