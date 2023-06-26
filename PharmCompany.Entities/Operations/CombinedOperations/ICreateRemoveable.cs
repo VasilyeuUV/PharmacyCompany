dcd28a8c89425d39f01e452c7874e1f50e4d5450
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-06-27 01:37:55 +0300

    sln: remove other projects

--- PharmCompany.Entities/Operations/CombinedOperations/ICreateRemoveable.cs
+++
@@ -1,9 +0,0 @@
-﻿using PharmCompany.Entities.Capabilities;
-
-namespace PharmCompany.Entities.Operations.CombinedOperations
-{
-    public interface ICreateRemoveable<T> : ICreateableAsync<T>, IRemoveable<T>
-        where T : class, IIdentifiable
-    {
-    }
-}
--- PharmCompany.ViewModels/UIViewModels/ConsoleAppViewModels/IConsoleAppViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PharmCompany.ViewModels.UIViewModels.ConsoleAppViewModels
-{
-    /// <summary>
-    /// Контракт вьюмодели для консольного приложения
-    /// </summary>
-    public interface IConsoleAppViewModel
-    {
-    }
-}
